6a7cddee920e7d81dda1bcfe5a471e0915635eb4
Valerio Pellegrino <pellegrino.valerio1@gmail.com>
2023-03-21 17:37:24 -0500

    Update store by adding product module file

--- src/store/index.js
+++ src/store/index.js
@@ -3,10 +3,13 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 import profile from './profile'
+import product from './product'
+
 
 export default new Vuex.Store({
   
   modules: {
-    profile
+      profile,
+      product
   }
 })
