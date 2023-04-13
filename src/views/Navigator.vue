09232427b936a8f54b689fe5d7b9af80f410e8a2
Valerio Pellegrino <pellegrino.valerio1@gmail.com>
2023-04-12 20:16:44 -0500

    Code Cleaning update

--- src/views/Navigator.vue
+++ src/views/Navigator.vue
@@ -1,31 +1,30 @@
 <template>
-    <div id="nav">
-         <router-link to="/">Home</router-link> !
-         <router-link to="/login">Login</router-link> ! 
-         <router-link to="/products">Products</router-link> ! 
-         <router-link to="/new-product">NEW PRODUCT</router-link> !
-         <span style="float:right;">
-         </span>
-    </div>
-
+  <div id="nav">
+    <router-link to="/">Home</router-link> !
+    <router-link to="/login">Login</router-link> !
+    <router-link to="/products">Products</router-link> !
+    <router-link to="/new-product">NEW PRODUCT</router-link> !
+    <span style="float:right;">
+    </span>
+  </div>
 </template>
 <script>
 export default {
- name: 'navigatorView',
- data (){
-   return {
-     
-   }
- },
- computed : {
+  name: 'navigatorView',
+  data() {
+    return {
 
- },
- methods : {
- 
- },
- components: {
- //  'profile-detail-component' : ProfileDetailComponent,
- //  'profile-component' : ProfileComponent 
- }
+    }
+  },
+  computed: {
+
+  },
+  methods: {
+
+  },
+  components: {
+    //  'profile-detail-component' : ProfileDetailComponent,
+    //  'profile-component' : ProfileComponent 
+  }
 }
 </script>
