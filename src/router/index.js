182585dc4268f1a076fcd87ff201485c7e791707
Valerio Pellegrino <pellegrino.valerio1@gmail.com>
2023-04-12 20:14:05 -0500

    Code Cleaning update

--- src/router/index.js
+++ src/router/index.js
@@ -1,48 +1,43 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import LoginView from '../views/LoginView.vue'
-import EmployeeView from '../views/EmployeeView.vue'
-import NewProductView from '../views/NewProductView.vue'
-
-Vue.use(VueRouter)
-
+import Vue from "vue";
+import VueRouter from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import LoginView from "../views/LoginView.vue";
+import ProductView from "../views/ProductView.vue";
+import NewProductView from "../views/NewProductView.vue";
+Vue.use(VueRouter);
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "home",
+    component: HomeView,
   },
   {
-    path: '/about',
-    name: 'about',
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: "/about",
+    name: "about",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
   },
   {
-    path: '/login',
-    name: 'login',
-    component: LoginView
+    path: "/login",
+    name: "login",
+    component: LoginView,
   },
   {
-    path: '/products',
-    name: 'products',
-    component: EmployeeView
-  }
+    path: "/products",
+    name: "products",
+    component: ProductView,
+  },
   /*,{
     path: '/pagination',
     name: 'pagination',
     component: PaginationView
-  }*/,
-  {
-    path: '/new-product',
-    name: 'new-product',
-    component: NewProductView
+  }*/ {
+    path: "/new-product",
+    name: "new-product",
+    component: NewProductView,
   },
-
-] 
-
+];
 const router = new VueRouter({
-  routes
-})
-
-export default router
+  routes,
+});
+export default router;
