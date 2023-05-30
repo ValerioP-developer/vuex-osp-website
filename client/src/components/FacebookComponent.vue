e29c867ec7ece817f8f337a05ac4be3ee19100c8
Valerio Pellegrino <pellegrino.valerio1@gmail.com>
2023-05-30 18:05:23 -0500

    Added FacebookComponent

---
+++ client/src/components/FacebookComponent.vue
@@ -0,0 +1,38 @@
+<template>
+  <span @click="facebookLink(url)">
+    <icon name="brands/facebook" color="#3B5998" :scale="scale"></icon>
+  </span>
+</template>
+
+<script>
+import objectToGetParams from '../utils/objectToGetParams';
+import 'vue-awesome/icons/brands/facebook';
+import Icon from 'vue-awesome/components/Icon';
+export default {
+  name: 'FacebookComponent',
+  components: {
+    Icon
+  },
+  methods: {
+    facebookLink(url) {
+      window.open(
+        `https://www.facebook.com/sharer/sharer.php` +
+        objectToGetParams({
+          u: url
+        }),
+        '__blank'
+      );
+    }
+  },
+  props: {
+    url: {
+      required: true,
+      type: String
+    },
+    scale: {
+      required: false,
+      type: String
+    }
+  }
+};
+</script>
