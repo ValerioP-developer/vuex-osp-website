528b7e328220ab2ae3c90287f8d0964768dc98e5
Valerio Pellegrino <pellegrino.valerio1@gmail.com>
2023-09-13 19:23:51 -0500

    update url server

--- client/src/config/env.js
+++ client/src/config/env.js
@@ -3,7 +3,8 @@
  */
 //const baseApiURL = 'https://api.instantwebtools.net'
 
-const baseApiURL = "http://localhost:8082/v1/";
-const stripeCheckoutAPI = "http://localhost:8082/v1/stripe";
+const baseApiURL = "https://vuex-server-side-869bc46c0f15.herokuapp.com/v1";
+const stripeCheckoutAPI =
+  "https://vuex-server-side-869bc46c0f15.herokuapp.com/v1/stripe";
 
 export { baseApiURL, stripeCheckoutAPI };
