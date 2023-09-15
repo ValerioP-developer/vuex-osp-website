7b62abf876eac345837c90f167934bf84c175b1a
Valerio Pellegrino <pellegrino.valerio1@gmail.com>
2023-09-15 18:44:20 -0500

    Update urls

--- client/src/config/env.js
+++ client/src/config/env.js
@@ -3,11 +3,8 @@
  */
 //const baseApiURL = 'https://api.instantwebtools.net'
 
-//const baseApiURL = "https://vuex-server-side-869bc46c0f15.herokuapp.com/v1";
-//const stripeCheckoutAPI =  "https://vuex-server-side-869bc46c0f15.herokuapp.com/v1/stripe";
-
-const baseApiURL = "https://full-blog-app-099c69300163.herokuapp.com/v1";
+const baseApiURL = "https://vuex-server-side-869bc46c0f15.herokuapp.com/v1";
 const stripeCheckoutAPI =
-  "https://full-blog-app-099c69300163.herokuapp.com/v1/stripe";
+  "https://vuex-server-side-869bc46c0f15.herokuapp.com/v1/stripe";
 
 export { baseApiURL, stripeCheckoutAPI };
