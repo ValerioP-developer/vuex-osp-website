a0d79280f8450189dde96ce90149a644f79a69ca
Valerio Pellegrino <pellegrino.valerio1@gmail.com>
2023-10-16 18:05:16 -0500

    Update main background pict.

--- client/src/components/TopContainer.vue
+++ client/src/components/TopContainer.vue
@@ -3,20 +3,20 @@
     <div class="jumbotron">
       <div class="container">
         <div v-if="route == 'home'">
-          <H1>
+          <span class="txt-top-container">
             {{ getMainHomeText }} <br>
             {{ getMainHomeText2 }}
-          </H1>
+          </span>
         </div>
         <div v-else-if="route == 'product'">
-          <H1>
+          <span class="txt-top-container">
             {{ getMainProductText }}
-          </H1>
+          </span>
         </div>
         <div v-else-if="route == 'blog'">
-          <H1>
+          <span class="txt-top-container">
             {{ getMainText }}
-          </H1>
+          </span>
         </div>
         <div v-else-if="route == 'about'">
           <img src="images/pvl-logo.jpg" id="profile" alt="logo">
@@ -69,4 +69,73 @@ export default {
   border-radius: 50%;
   width: 200px;
 }
+
+.jumbotron {
+  background: red;
+  height: 800px;
+}
+
+
+/** Medium devices (tablets, 768px and up)  */
+@media (min-width: 768px) {
+  .jumbotron {
+    background: red;
+    height: 600px;
+  }
+
+  .txt-top-container {
+    font-size: 45px;
+  }
+}
+
+@media (min-width: 576px) and (max-width: 767px) {
+  .jumbotron {
+    background: red;
+    height: 400px;
+  }
+
+  .txt-top-container {
+    font-size: 35px;
+  }
+}
+
+@media (min-width: 250px) and (max-width: 575.99px) {
+  .jumbotron {
+    background: red;
+    height: 400px;
+  }
+
+  .txt-top-container {
+    font-size: 22px;
+  }
+}
+
+
+@media (min-width: 0) and (max-width: 249.99px) {
+  .jumbotron {
+    background: red;
+    height: 400px;
+  }
+
+  .txt-top-container {
+    font-size: 20px;
+  }
+}
+
+/*
+@media (min-width: 576px) {
+  .jumbotron {
+    background: red;
+    height: 40%;
+  }
+
+}
+
+@media (max-width: 575px) {
+  .jumbotron {
+    background: red;
+    height: 40%;
+  }
+
+}*/
 </style>
