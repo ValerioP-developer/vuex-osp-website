54ed31c5c5537654e4426532ffffc3f73e9ce29f
Valerio Pellegrino <pellegrino.valerio1@gmail.com>
2023-10-16 21:46:35 -0500

    Add PaymentInfo page after checkout

---
+++ client/src/views/PaymentInfo.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="payment">
+        <TopContainer :route=route> </TopContainer>
+        <div class="container">
+            <h1> Payment successfull done! check your email </h1>
+
+        </div>
+        <BottomContainer></BottomContainer>
+    </div>
+</template>
+<script>
+import TopContainer from '@/components/TopContainer.vue'
+import BottomContainer from '@/components/BottomContainer.vue'
+export default {
+    name: 'PaymentInfo',
+    components: {
+        TopContainer,
+        BottomContainer
+    },
+
+    data() {
+        return {
+            route: 'product',
+        }
+    },
+    created() {
+        //alert("name  " + this.$route.query.name + "items : " + this.$route.query.quantity) // outputs 
+        setTimeout(() => this.$router.push({ path: '/' }), 5000);
+    },
+    methods: {
+        pageRedirect() {
+            var delay = 5000
+            setTimeout(function () {
+                alert("##### REDIRECT INSTRUCTION ######")
+                this.$router.push('/')
+            }, delay);
+        }
+    }
+}
+</script>
+<style></style>
